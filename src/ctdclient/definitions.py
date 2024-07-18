7c58dcca17c589485fec7c18f6869da5e8f03807
Emil Michels <emil.michels@io-warnemuende.de>
2024-07-18 14:32:47 +0200

    Setup of tufup for a self-updating procedure

--- src/ctdclient/definitions.py
+++ src/ctdclient/definitions.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 from pathlib import Path
 
 from ctdclient.utils import get_config_path
@@ -6,3 +7,12 @@ from ctdclient.utils import get_config_path
 ROOT_PATH = Path(__file__).absolute().parents[2]
 CONFIG_PATH = get_config_path()
 THEMES_PATH = ROOT_PATH.joinpath("src/ctdclient/view/ctktheme.json")
+VERSION = importlib.metadata.version("ctdclient")
+
+
+# update specifics
+INSTALL_DIR = ROOT_PATH.joinpath("updates")
+TUFUP_METADATA = INSTALL_DIR.joinpath("metadata")
+TUFUP_TARGET = INSTALL_DIR.joinpath("target")
+METADATA_URL = "http://localhost:8000/metadata/"
+TARGET_URL = "http://localhost:8000/target/"

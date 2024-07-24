b1e2cdf9c2a09dc735a53b3e245d5d2a522e558f
Emil Michels <emil.michels@io-warnemuende.de>
2024-07-24 11:31:49 +0200

    Added a script to handle client updating on windows

---
+++ new_build.py
@@ -0,0 +1,55 @@
+#!.venv/Scripts/python
+from pathlib import Path
+import sys
+import importlib.metadata
+import argparse
+import subprocess
+import PyInstaller.__main__
+from tufup.repo import Repository
+
+def main(target_dir: str, release: bool):
+    print('\nRunning \"poetry install\" to ensure latest versions and bump version number.', flush=True)
+    ps = subprocess.Popen(['poetry', 'install', '--without', 'dev'])
+    ps.wait()
+
+    print('\nRunning PyInstaller to create a new bundled executable.', flush=True)
+    PyInstaller.__main__.run([
+        '-y',
+        'windows.spec',
+        f'--distpath {target_dir}',
+    ])
+
+    if release:
+        # find the bundled ctdclient.exe and its files
+        print('\nRunning tufup to create a new release.', flush=True)
+        target_dir = Path(target_dir)
+        if target_dir.exists():
+            repo = Repository.from_config()
+            repo.add_bundle(
+                new_bundle_dir=target_dir,
+                new_version= importlib.metadata.version("ctdclient"),
+                custom_metadata={},
+                skip_patch=True)
+            print('\nPublish tufup changes.', flush=True)
+            repo.publish_changes(private_key_dirs=['tufup_keys'])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--target_dir',
+        '-t',
+        type=str,
+        default='dist/ctdclient',
+        help="The target dir for the final exe, usually dist/ctdclient"
+    )
+    parser.add_argument(
+        '--release',
+        '-r',
+        action="store_true",
+        default=False,
+        help="Whether to use tufup to publish a new release target. DEFAULT=False"
+        )
+    args = parser.parse_args()
+    main(args.target_dir, args.release)
+    sys.exit(0)

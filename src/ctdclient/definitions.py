9d39e8750e2ef32445076e0b248b8b9c8d2f1c3c
Emil Michels <emil.michels@io-warnemuende.de>
2024-07-25 17:51:56 +0200

    Moved all new configuration parameters into ctdclient.toml

--- src/ctdclient/definitions.py
+++ src/ctdclient/definitions.py
@@ -1,53 +1,17 @@
 import sys
 import importlib.metadata
 from pathlib import Path
-import argparse
+from ctdclient.configurationhandler import ConfigurationFile
 
 from ctdclient.utils import get_config_path
 
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument(
-#     '--dship_present',
-#     '-s',
-#     # type=bool,
-#     default=True,
-#     action="store_false",
-#     help="Whether on a ship with dship running. DEFAULT=True"
-#     )
-# parser.add_argument(
-#     '--dev',
-#     '-d',
-#     # type=bool,
-#     default=False,
-#     action="store_true",
-#     help="Whether in a development environment with respective paths. DEFAULT=False"
-#     )
-# parser.add_argument(
-#     '--update_server',
-#     '-u',
-#     type=str,
-#     default="http://localhost:8000/",
-#     help="The adress of the server from which to fetch updates."
-# ) 
-# args = parser.parse_args()
-# 
-# DSHIP = args.dship_present
-# DEV = args.dev
-# SERVER = args.update_server
-DSHIP = False
-DEV = False
-SERVER = "http://localhost:8000/"
-UPDATING = True
-
-ROOT_PATH = Path(__file__).absolute().parents[2]
 if getattr(sys, 'frozen', False):
+    ROOT_PATH = Path(sys.executable).parent
     RESSOURCES_PATH = Path(sys._MEIPASS)
-    FROZEN = True
 else:
-    RESSOURCES_PATH = ROOT_PATH
-    FROZEN = False
+    RESSOURCES_PATH = ROOT_PATH = Path(__file__).absolute().parents[2]
 CONFIG_PATH = get_config_path(ROOT_PATH, RESSOURCES_PATH)
+config = ConfigurationFile(CONFIG_PATH)
 VERSION = importlib.metadata.version("ctdclient")
 THEMES_PATH = RESSOURCES_PATH.joinpath("ctktheme.json")
 ICON_PATH = RESSOURCES_PATH.joinpath("icon.ico")
@@ -56,6 +20,9 @@ PROCESSING_TEMPLATE_PATH = RESSOURCES_PATH.joinpath("templates", "processing_tem
 # update specifics
 INSTALL_DIR = RESSOURCES_PATH.joinpath("updates")
 TUFUP_METADATA = INSTALL_DIR.joinpath("metadata")
-TUFUP_TARGET = INSTALL_DIR.joinpath("target")
-METADATA_URL = f"{SERVER}metadata/"
-TARGET_URL = f"{SERVER}targets/"
+TUFUP_TARGET = INSTALL_DIR.joinpath("targets")
+# ensure, that directory exists
+if not TUFUP_TARGET.exists():
+    TUFUP_TARGET.mkdir(parents=True)
+METADATA_URL = f"{config.server}metadata/"
+TARGET_URL = f"{config.server}targets/"

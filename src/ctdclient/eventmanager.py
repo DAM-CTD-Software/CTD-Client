87af954af06d181ba186a62cb37172abb7d311e0
Author Emil Michels <emil.michels@io-warnemuende.de>
2024-12-30 11:06:50 +0100

    Removed example usage

--- src/ctdclient/eventmanager.py
+++ src/ctdclient/eventmanager.py
@@ -28,23 +28,3 @@ class EventManager:
         if event_name in self._listeners:
             for callback in self._listeners[event_name]:
                 callback(*args, **kwargs)
-
-
-class ModelA:
-    def __init__(self, event_manager):
-        self.event_manager = event_manager
-
-    def do_something(self):
-        # Notify listeners about the event
-        print("ModelA: Doing something and notifying listeners...")
-        self.event_manager.publish("event_happened", data="Hello from ModelA")
-
-
-class ModelB:
-    def __init__(self, event_manager):
-        self.event_manager = event_manager
-        # Subscribe to the "event_happened" event
-        self.event_manager.subscribe("event_happened", self.on_event_happened)
-
-    def on_event_happened(self, data):
-        print(f"ModelB: Received event with data: {data}")

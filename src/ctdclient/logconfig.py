c0ba38b6a0f8036b6783372cd4819fc265a2cbc2
Emil Michels <emil.michels@io-warnemuende.de>
2025-08-07 16:23:23 +0200

    Feature: fine-grained log control and email sending of log messages

--- src/ctdclient/logconfig.py
+++ src/ctdclient/logconfig.py
@@ -1,8 +1,15 @@
 import logging
+import mimetypes
 import queue
+import smtplib
+import sys
+from datetime import datetime
+from datetime import timezone
+from email.message import EmailMessage
 
 import customtkinter as ctk
 from ctdclient.definitions import config
+from ctdclient.definitions import cruise_name
 from CTkMessagebox import CTkMessagebox
 
 
@@ -28,14 +35,21 @@ class LoggingConfig:
             level=loglevel,
             format=format,
             datefmt=datefmt,
-            handlers=[
-                logging.FileHandler(self.log_file),
-                logging.StreamHandler(),
-                self.QueueHandler(self.log_queue),
-            ],
+            handlers=[logging.FileHandler(self.log_file)],
         )
 
         self.logger = logging.getLogger(self.logger_name)
+        # log to console when not running as exe
+        if getattr(sys, "frozen", True):
+            sh = logging.StreamHandler()
+            sh.setLevel(logging.DEBUG)
+            formatter = logging.Formatter("%(name)s - %(message)s")
+            sh.setFormatter(formatter)
+            logging.getLogger("").addHandler(sh)
+        # log to GUI with restricted log level
+        qh = self.QueueHandler(self.log_queue)
+        qh.setLevel(logging.DEBUG if config.debugging else logging.ERROR)
+        logging.getLogger("").addHandler(qh)
         self.root.after(1000, self.process_log_queue)
 
     class QueueHandler(logging.Handler):
@@ -54,12 +68,57 @@ class LoggingConfig:
             while True:
                 record = self.log_queue.get(block=False)
                 if record:
-                    CTkMessagebox(
+                    answer = CTkMessagebox(
                         title=f"Log Message: {record.name}: {record.levelname}",
                         icon="cancel",
                         message=record.getMessage(),
                         option_1="Ok",
+                        option_2="Send as email",
                     )
+                    if answer.get() == "Send as email":
+                        self.send_email(record)
         except queue.Empty:
             pass
         self.root.after(1000, self.process_log_queue)
+
+    def send_email(self, record):
+        try:
+            smtp_server = config.email_config["smtp_server"]
+            smtp_port = config.email_config["smtp_port"]
+            assert len(smtp_server) and len(str(smtp_port))
+        except (KeyError, AssertionError):
+            self.logger.error(
+                "Could not send email, because of missing smtp server and/or port information."
+            )
+            return
+        msg = EmailMessage()
+        timestamp = datetime.now(tz=timezone.utc).strftime("%y-%m-%d %H:%M:%S")
+        msg.set_content(
+            f"{timestamp} - {record.name} - {record.levelname} - {record.getMessage()}"
+        )
+        msg["Subject"] = f"CTD-Client Error Log from {cruise_name}"
+        msg["From"] = config.email_config["smtp_email"]
+        msg["To"] = "emil.michels@io-warnemuende.de"
+        for file in ["ctdclient.toml", "ctdclient.log"]:
+            try:
+                mime_type, _ = mimetypes.guess_type(file)
+                if mime_type is None:
+                    mime_type = "application/octet-stream"
+                main_type, sub_type = mime_type.split("/", 1)
+                with open(file, "rb") as data:
+                    msg.add_attachment(
+                        data.read(),
+                        maintype=main_type,
+                        subtype=sub_type,
+                        filename=file,
+                    )
+            except Exception:
+                continue
+        with smtplib.SMTP(smtp_server, int(smtp_port)) as server:
+            server.starttls()
+            try:
+                server.send_message(msg)
+            except smtplib.SMTPRecipientsRefused as error:
+                self.logger.error(f"Credentials needed to send email: {error}")
+            else:
+                self.logger.info(f"Email sent to {msg['To']}")

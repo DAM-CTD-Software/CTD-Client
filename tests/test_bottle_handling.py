bd7a0015a68bc37973b76187995582600dc025ee
Emil Michels <emil.michels@io-warnemuende.de>
2025-09-03 13:25:02 +0200

    Tests: updated test for more sophisticated algorithm

--- tests/test_bottle_handling.py
+++ tests/test_bottle_handling.py
@@ -43,13 +43,14 @@ def test_comma2dot(psa):
 
 
 def test_same_depth_mapping(config):
-    test_data = {1: '1', 2: '2', 3: '1'}
+    test_data = {1: '1', 2: '1.5', 3: '1.2'}
     config.minimum_bottle_diff = 0.4
     config.number_of_bottles = len(test_data)
     bottles = BottleClosingDepths(config)
     bottles.check_bottle_data(test_data)
-    assert bottles[1] == '0.8'
+    assert bottles[1] == '0.9'
     assert bottles[3] == '1.2'
+    assert bottles[2] == '1.6'
 
     config.minimum_bottle_diff = 0
     bottles = BottleClosingDepths(config)

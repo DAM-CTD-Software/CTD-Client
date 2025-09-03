1294d7903875e1a234947d7a46e5e86f52635be3
Emil Michels <emil.michels@io-warnemuende.de>
2025-09-03 17:12:05 +0200

    Tests: test more complex cases

--- tests/test_bottle_handling.py
+++ tests/test_bottle_handling.py
@@ -43,19 +43,17 @@ def test_comma2dot(psa):
 
 
 def test_same_depth_mapping(config):
-    test_data = {1: '1', 2: '1.5', 3: '1.2'}
+    test_data = {1: '1.9', 2: 'test', 3: '1.2', 4: '1,6'}
     config.minimum_bottle_diff = 0.4
     config.number_of_bottles = len(test_data)
     bottles = BottleClosingDepths(config)
     bottles.check_bottle_data(test_data)
-    assert bottles[1] == '0.9'
-    assert bottles[3] == '1.2'
-    assert bottles[2] == '1.6'
+    assert bottles == {1: '2.0' , 2: '', 3: '1.2', 4: '1.6'}
 
     config.minimum_bottle_diff = 0
     bottles = BottleClosingDepths(config)
     bottles.check_bottle_data(test_data)
-    assert bottles[1] == '1.0'
+    assert bottles[1] == '1.9'
 
 
 def test_more_than_two_same_depth_mappings(config, caplog):

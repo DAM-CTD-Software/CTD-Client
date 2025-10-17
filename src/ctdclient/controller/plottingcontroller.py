c2ed8cdf1540f4beeccf4e718db9d7755f771773
Emil Michels <emil.michels@io-warnemuende.de>
2025-10-17 16:25:59 +0200

    Feature: added plotting functionality from processing visualize

---
+++ src/ctdclient/controller/plottingcontroller.py
@@ -0,0 +1,26 @@
+from ctdclient.controller.Controller import Controller
+from ctdclient.model.plotting import Plotting
+from ctdclient.view.plotting import PlottingFrame
+
+
+class PlottingController(Controller):
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.model: Plotting
+        self.view: PlottingFrame
+        self.view.add_callback("plot_file", self.plot_file)
+        self.view.add_callback("plot_cruise", self.plot_cruise)
+        self.view.add_callback("update_auto_plot", self.update_auto_plot)
+
+    def plot_file(self, file: str):
+        self.model.plot_file(file)
+
+    def plot_cruise(self, directory: str):
+        self.model.plot_cruise(directory)
+
+    def update_auto_plot(self, auto_plot: str):
+        self.model.toggle_auto_plot(bool(auto_plot))
